1c31b88cdac1b810809646c639b7cf9181e4c4db
Nerjal Nosk <nosk.nerjal@gmx.com>
2024-01-01 03:55:33 +0100

    Traveler's Backpack x Trinkets compat

---
+++ src/main/java/hibi/blahaj/mixin/compat/travelersbackpack/and/trinkets/present/client/TravelersBackpackWearableModelMixin.java
@@ -0,0 +1,27 @@
+package hibi.blahaj.mixin.compat.travelersbackpack.and.trinkets.present.client;
+
+import com.tiviacz.travelersbackpack.client.model.TravelersBackpackWearableModel;
+import hibi.blahaj.compat.TrinketPlushRenderer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.client.render.VertexConsumer;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.entity.LivingEntity;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Environment(EnvType.CLIENT)
+@Mixin(TravelersBackpackWearableModel.class)
+public abstract class TravelersBackpackWearableModelMixin {
+    @Shadow @Final private LivingEntity livingEntity;
+
+    @Inject(method = "render", at = @At("HEAD"), cancellable = true)
+    private void onRenderHeadInjector(MatrixStack matrices, VertexConsumer vertices, int light, int overlay, float red,
+                                      float green, float blue, float alpha, CallbackInfo ci) {
+        if (TrinketPlushRenderer.hasCuddlyInSlot(this.livingEntity, "chest", "back", "cape")) ci.cancel();
+    }
+}

904543035dd8b1c09f626c2261c497a0ba71d579
Nerjal Nosk <nosk.nerjal@gmx.com>
2024-05-30 14:06:15 +0200

    Added Blahaj Bass item & animator

---
+++ src/main/java/mc/recraftors/blahaj/mixin/compat/immersive_melodies/present/ItemsMixin.java
@@ -0,0 +1,18 @@
+package mc.recraftors.blahaj.mixin.compat.immersive_melodies.present;
+
+import immersive_melodies.Items;
+import mc.recraftors.blahaj.Blahaj;
+import mc.recraftors.blahaj.compat.BlahajBassAnimator;
+import org.joml.Vector3f;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(Items.class)
+public interface ItemsMixin {
+    @Inject(method = "<clinit>", at = @At("TAIL"))
+    private static void blahajClinitInjector(CallbackInfo ci) {
+        Items.register(Blahaj.MOD_ID, "blahaj_bass", new BlahajBassAnimator(), 200L, new Vector3f(0, .1f, .5f));
+    }
+}

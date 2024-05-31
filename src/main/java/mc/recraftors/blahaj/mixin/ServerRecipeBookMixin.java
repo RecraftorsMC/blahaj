f5afc66e35b154c76b31f73398360eee5045ba30
Nerjal Nosk <nosk.nerjal@gmx.com>
2024-05-31 03:33:29 +0200

    1.20.2 no unlock patch

--- src/main/java/mc/recraftors/blahaj/mixin/ServerRecipeBookMixin.java
+++ src/main/java/mc/recraftors/blahaj/mixin/ServerRecipeBookMixin.java
@@ -1,7 +1,7 @@
 package mc.recraftors.blahaj.mixin;
 
 import mc.recraftors.blahaj.Blahaj;
-import net.minecraft.recipe.Recipe;
+import net.minecraft.recipe.RecipeEntry;
 import net.minecraft.server.network.ServerRecipeBook;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -13,9 +13,9 @@ import java.util.Collection;
 @Mixin(ServerRecipeBook.class)
 public class ServerRecipeBookMixin {
     @ModifyVariable(method = "unlockRecipes", argsOnly = true, at = @At("HEAD"))
-    private Collection<Recipe<?>> glitchNoUnlock(Collection<Recipe<?>> collection) {
+    private Collection<RecipeEntry<?>> glitchNoUnlock(Collection<RecipeEntry<?>> collection) {
         collection = new ArrayList<>(collection);
-        collection.removeIf(r -> Blahaj.isHidden(r.getId()));
+        collection.removeIf(r -> Blahaj.isHidden(r.id()));
         return collection;
     }
 }

586b3a64a37175d4226b9dcc20d0274232264966
Nerjal Nosk <nosk.nerjal@gmx.com>
2024-05-31 02:33:28 +0200

    No unlock hidden recipes
    (cherry picked from commit 1ddf63e74dcfa9653c0e4ced164ff8388bd5e472)

---
+++ src/main/java/mc/recraftors/blahaj/mixin/ServerRecipeBookMixin.java
@@ -0,0 +1,21 @@
+package mc.recraftors.blahaj.mixin;
+
+import mc.recraftors.blahaj.Blahaj;
+import net.minecraft.recipe.Recipe;
+import net.minecraft.server.network.ServerRecipeBook;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+@Mixin(ServerRecipeBook.class)
+public class ServerRecipeBookMixin {
+    @ModifyVariable(method = "unlockRecipes", argsOnly = true, at = @At("STORE"))
+    private Collection<Recipe<?>> glitchNoUnlock(Collection<Recipe<?>> collection) {
+        collection = new ArrayList<>(collection);
+        collection.removeIf(r -> Blahaj.isHidden(r.getId()));
+        return collection;
+    }
+}
